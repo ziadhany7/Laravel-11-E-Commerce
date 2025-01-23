7114e0b93c1427ad8cf64aea974ab64e5d57d421
ziadhany7 <ziadhanywadea5@gmail.com>
2025-01-24 01:35:50 +0200

    Added product sorting and pagination feature with customizable products per page option

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -7,13 +7,41 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index(){
-        $products = Product::orderBy('created_at','DESC')->paginate(12);
-        return view('shop.shop-home',compact('products'));
+    public function index(Request $request)
+    {
+        $size = $request->query('size') ? $request->query('size') : 12;
+        $o_column = "";
+        $o_order = "";
+        $order = $request->query('order') ? $request->query('order') : -1;
+        switch ($order) {
+            case 1:
+                $o_column = 'created_at';
+                $o_order = 'DESC';
+                break;
+            case 2:
+                $o_column = 'created_at';
+                $o_order = 'ASC';
+                break;
+            case 3:
+                $o_column = 'sale_price';
+                $o_order = 'ASC';
+                break;
+            case 4:
+                $o_column = 'sale_price';
+                $o_order = 'DESC';
+                break;
+            default:
+                $o_column = 'id';
+                $o_order = 'DESC';
+                break;
+        }
+        $products = Product::orderBy($o_column,$o_order)->paginate($size);
+        return view('shop.shop-home', compact('products', 'size', 'order'));
     }
-    public function product_details($product_slug){
-        $product = Product::where('slug',$product_slug)->first();
-        $related_products= Product::where('slug','<>','$product_slug')->get()->take(8);
-        return View('shop.details',compact('product','related_products'));
+    public function product_details($product_slug)
+    {
+        $product = Product::where('slug', $product_slug)->first();
+        $related_products = Product::where('slug', '<>', '$product_slug')->get()->take(8);
+        return View('shop.details', compact('product', 'related_products'));
     }
 }
--- resources/views/shop/shop-home.blade.php
+++ resources/views/shop/shop-home.blade.php
@@ -366,16 +366,20 @@
                     <div
                         class="shop-acs d-flex align-items-center justify-content-between justify-content-md-end flex-grow-1">
                         <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0"
-                            aria-label="Sort Items" name="total-number">
-                            <option selected>Default Sorting</option>
-                            <option value="1">Featured</option>
-                            <option value="2">Best selling</option>
-                            <option value="3">Alphabetically, A-Z</option>
-                            <option value="3">Alphabetically, Z-A</option>
-                            <option value="3">Price, low to high</option>
-                            <option value="3">Price, high to low</option>
-                            <option value="3">Date, old to new</option>
-                            <option value="3">Date, new to old</option>
+                            aria-label="Page Size" id="pagesize" name="pagesize" style="margin-right: 5%">
+                            <option value="12" {{ $size == 12 ? 'selected' : '' }}>Show</option>
+                            <option value="24" {{ $size == 24 ? 'selected' : '' }}>24</option>
+                            <option value="48" {{ $size == 48 ? 'selected' : '' }}>48</option>
+                            <option value="102" {{ $size == 102 ? 'selected' : '' }}>102</option>
+                        </select>
+
+                        <select class="shop-acs__select form-select w-auto border-0 py-0 order-1 order-md-0"
+                            aria-label="Sort Items" name="orderby" id="orderby">
+                            <option value="-1" {{ $order == -1 ? 'selected' : '' }}>Default Sorting</option>
+                            <option value="1" {{ $order == 1 ? 'selected' : '' }}>Date, New To Old</option>
+                            <option value="2" {{ $order == 2 ? 'selected' : '' }}>Date, Old To New</option>
+                            <option value="3" {{ $order == 3 ? 'selected' : '' }}>Price, Low To High</option>
+                            <option value="4" {{ $order == 4 ? 'selected' : '' }}>Price, High To Low</option>
                         </select>
 
                         <div class="shop-asc__seprator mx-3 bg-light d-none d-md-block order-md-0"></div>
@@ -516,9 +520,31 @@
 
                 <div class="divider"></div>
                 <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination">
-                    {{ $products->links('pagination::bootstrap-5') }}
+                    {{ $products-> withQueryString()->links('pagination::bootstrap-5') }}
                 </div>
             </div>
         </section>
     </main>
+<form id="frmfilter" method="GET" action="{{route("shop.index")}}">
+    <input type="hidden" name="page" value="{{$products->currentPage()}}">
+    <input type="hidden" name="size" id="size" value="{{$size}}" />
+    <input type="hidden" name="order" id="order" value="{{$order}}" />
+</form>
 @endsection
+
+@push('scripts')
+    <script>
+        $(function(){
+            $("#pagesize").on("change",function(){
+                $("#size").val($("#pagesize option:selected").val());
+                $("#frmfilter").submit();
+            })
+        });
+        $(function(){
+            $("#orderby").on("change",function(){
+                $("#order").val($("#orderby option:selected").val());
+                $("#frmfilter").submit();
+            })
+        });
+    </script>
+@endpush
