c15a6b4a7e352b0f7c060c80befad5048d941ed8
ziadhany7 <ziadhanywadea5@gmail.com>
2025-02-16 00:26:27 +0200

    Created the model, migration, and established relationships to manage order processing and data integrity.

---
+++ app/Models/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function orderItem(){
+        return $this->hasMany(OrderItem::class);
+    }
+    public function transaction(){
+        return $this->hasOne(Transaction::class);
+    }
+}
