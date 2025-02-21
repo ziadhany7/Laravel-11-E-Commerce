1c7f39ace12ea22cbb8a27d1bdcfb94c48dc6589
ziadhany7 <ziadhanywadea5@gmail.com>
2025-02-21 19:41:24 +0200

    Enhanced the admin dashboard to be dynamic, displaying real-time data updates for improved management and insights.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,6 +13,7 @@ use App\Models\Slide;
 use Carbon\Carbon;
 use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Intervention\Image\Laravel\Facades\Image;
@@ -22,7 +23,43 @@ class AdminController extends Controller
 {
     public function index()
     {
-        return view("admin.index");
+        $orders = Order::orderBy('created_at', 'DESC')->get()->take(5);
+        $dashboardDatas = DB::select("Select sum(total) As TotalAmount,
+                                        sum(if(status='ordered',total,0)) As TotalOrderedAmount,
+                                        sum(if(status='delivered',total,0)) As TotalDeliveredAmount,
+                                        sum(if(status='canceled',total,0)) As TotalCanceledAmount,
+                                        Count(*) As Total,
+                                        sum(if(status='ordered',1,0)) As TotalOrdered,
+                                        sum(if(status='delivered',1,0)) As TotalDelivered,
+                                        sum(if(status='canceled',1,0)) As TotalCanceled
+                                        From Orders
+                                    ");
+        $monthlyDatas = DB::select("SELECT M.id As MonthNo, M.name As MonthName,
+                                    IFNULL(D.TotalAmount,0) As TotalAmount,
+                                    IFNULL(D.TotalOrderedAmount,0) As TotalOrderedAmount,
+                                    IFNULL(D.TotalDeliveredAmount,0) As TotalDeliveredAmount,
+                                    IFNULL(D.TotalCanceledAmount,0) As TotalCanceledAmount FROM month_names M
+                                    LEFT JOIN (Select DATE_FORMAT(created_at, '%b') As MonthName,
+                                    MONTH(created_at) As MonthNo,
+                                    sum(total) As TotalAmount,
+                                    sum(if(status='ordered',total,0)) As TotalOrderedAmount,
+                                    sum(if(status='delivered',total,0)) As TotalDeliveredAmount,
+                                    sum(if(status='canceled',total,0)) As TotalCanceledAmount
+                                    From Orders WHERE YEAR(created_at)=YEAR(NOW()) GROUP BY YEAR(created_at), MONTH(created_at) , DATE_FORMAT(created_at, '%b')
+                                    Order By MONTH(created_at)) D On D.MonthNo=M.id");
+
+        $AmountM = implode(',', collect($monthlyDatas)->pluck('TotalAmount')->toArray());
+        $OrderedAmountM = implode(',', collect($monthlyDatas)->pluck('TotalOrderedAmount')->toArray());
+        $DeliveredAmountM = implode(',', collect($monthlyDatas)->pluck('TotalDeliveredAmount')->toArray());
+        $CanceledAmountM = implode(',', collect($monthlyDatas)->pluck('TotalCanceledAmount')->toArray());
+
+        $TotalAmount = collect($monthlyDatas)->sum('TotalAmount');
+        $TotalOrderedAmount = collect($monthlyDatas)->sum('TotalOrderedAmount');
+        $TotalDeliveredAmount = collect($monthlyDatas)->sum('TotalDeliveredAmount');
+        $TotalCanceledAmount = collect($monthlyDatas)->sum('TotalCanceledAmount');
+
+
+        return view("admin.index", compact('orders', 'dashboardDatas','AmountM','OrderedAmountM','DeliveredAmountM','CanceledAmountM','TotalAmount','TotalOrderedAmount','TotalDeliveredAmount','TotalCanceledAmount'));
 
         //Purpose of index Method : Shows the main admin page.
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\MonthName;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            MonthSeeder::class
+        ]);
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        
     }
 }
